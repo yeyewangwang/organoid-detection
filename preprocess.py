78a92aad8727254b8a946603167c0328f7ab9baa
Ye Wang <ye_wang2@brown.edu>
2022-04-20 21:38:57 -0400

    Add resize

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,10 @@
 #File for preprocessing the organoid images
+
+from skimage import io, img_as_float32
+from skimage.transform import resize
+
 from hyperparameters import img_size
 
 def resize(image):
-    pass
+    return resize(image, (img_size, img_size))
+
