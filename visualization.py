f87a08d8379c6f5a4c15aa04294255eb091cc520
Ye Wang <ye_wang2@brown.edu>
2022-05-08 16:37:25 -0400

    Print out actual box indices

--- visualization.py
+++ visualization.py
@@ -9,16 +9,17 @@ def plot_boxes(img, true_yxyxs, pred_yxyxs):
     for yxyx in true_yxyxs:
         yxyx = yxyx.numpy()
         print((yxyx[1], yxyx[0]), (yxyx[3], yxyx[2]))
-        cv2.rectangle(img, (yxyx[1], yxyx[0]), (yxyx[3], yxyx[2]),  (100, 100, 0))
-    print("Predictedg boxes")
+        # cv2.rectangle(img, (yxyx[1], yxyx[0]), (yxyx[3], yxyx[2]),  (100, 100, 0))
+    print("Predicted boxes")
     for yxyx in pred_yxyxs:
         yxyx = yxyx.numpy()
         print((yxyx[1], yxyx[0]), (yxyx[3], yxyx[2]))
 
-        cv2.rectangle(img, (yxyx[1], yxyx[0]), (yxyx[3], yxyx[2]), (255, 128, 0), 2)
-    plt.imshow(img)
-    plt.show()
-    plt.pause(2)
-    plt.close()
+         # cv2.rectangle(img, (yxyx[1], yxyx[0]), (yxyx[3], yxyx[2]), (255, 128, 0), 2)
+    # TODO: visualize images
+    # plt.imshow(img)
+    # plt.show()
+    # plt.pause(2)
+    # plt.close()
 
 
